ce47e07531ffcdc132597719d5b6ab48e566791b
khashika narain <khashika.narain@gmail.com>
2024-06-26 09:19:37 -0500

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Sruti Narain on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
